7b3af940826435572c718696eff0156f784d7147
emyio <emyio@192.168.1.108>
2019-10-06 22:48:32 +0300

    First Commit

---
+++ SeleniumProject/src/com/airdep/pages/LoginPage.java
@@ -0,0 +1,23 @@
+package com.airdep.pages;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class LoginPage {
+	@Test
+	public void verifyOpenPage(){
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://opensource-demo.orangehrmlive.com/");
+	driver.manage().window().maximize();
+	
+	}
+	@Test
+	public void verifyElement() {
+		WebDriver driver = new ChromeDriver();
+		driver.findElement(By.id("wrapper"));
+		driver.quit();
+		
+		
+	}
+}
